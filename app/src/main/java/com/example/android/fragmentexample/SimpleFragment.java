bfad61b809a7bcd24083e5db32f842b5f530b072
jordangibson1 <gibsonj22@hanover.edu>
2020-04-10 18:43:31 -0400

    Task 1 completed, radio buttons and fragment added

--- app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +15,8 @@ import android.view.ViewGroup;
  */
 public class SimpleFragment extends Fragment {
 
+    private static final int YES = 0;
+    private static final int NO = 1;
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -22,8 +26,33 @@ public class SimpleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple2, container, false);
+        // Inflate the layout for this fragment.
+        final View rootView = inflater.inflate(R.layout.fragment_simple2, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+          View radioButton = radioGroup.findViewById(checkedId);
+          int index = radioGroup.indexOfChild(radioButton);
+          TextView textView =
+                  rootView.findViewById(R.id.fragment_header);
+          switch (index) {
+              case YES: // User chose "Yes."
+                  textView.setText(R.string.yes_message);
+                  break;
+              case NO: // User chose "No."
+                  textView.setText(R.string.no_message);
+                  break;
+              default: // No choice made.
+                  // Do nothing.
+                  break;
+          }
+        }
+        });
+
+        // Return the View for the fragment's UI.
+        return rootView;
     }
 
 }
