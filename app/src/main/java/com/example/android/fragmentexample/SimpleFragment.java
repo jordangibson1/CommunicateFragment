9d0c8fd274679e85c13215ecca1c02605390da8a
jordangibson1 <gibsonj22@hanover.edu>
2020-04-10 19:10:20 -0400

    Task 2 Completed on FragmentExample2, Dynamically added fragment to activity and methods to update it accordingly

--- app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -24,6 +24,10 @@ public class SimpleFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
